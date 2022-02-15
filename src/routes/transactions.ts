eff61c10f829cf19a5d7cbd62536edc58d7aec0b
Ghassan Hallaq <gus.hallaq@gmail.com>
2022-02-15 07:04:57 -0500

    add status, slot to transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { getDetailTransactions, getVault, parseTx, addDeposit, addWithdraw, addBorrow, addPayback, addStake, addSwap, addReward } from '../api/transactions'
+import { getDetailTransactions, getVault, parseTx, parseTxsignatures, addDeposit, addWithdraw, addBorrow, addPayback, addStake, addSwap, addReward } from '../api/transactions'
 import { UserRole } from '../models/model';
 import { isNotSafe } from '../utils/utils';
 import { authorize } from '../middlewares/auth';
@@ -20,6 +20,13 @@ router.get('/:wallet_id/vault', async function (req: Request, res: Response) {
     });
 })
 
+router.get('/:wallet_id/signatures', async function (req: Request, res: Response) {
+
+    let result = await parseTxsignatures(req.params.wallet_id, function (result) {
+        res.send(JSON.stringify(result));
+    });
+})
+
 router.get('/:wallet_id/:signature', async function (req: Request, res: Response) {
 
     let result = await parseTx(req.params.wallet_id, req.params.signature, function (result) {
@@ -28,7 +35,7 @@ router.get('/:wallet_id/:signature', async function (req: Request, res: Response
 })
 
 router.post('/:wallet_id/deposit', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-    const keylist: string[] = ['transaction_id', 'address_id', 'amount'];
+    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
     if (isNotSafe(keylist, req.body)) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
@@ -37,7 +44,7 @@ router.post('/:wallet_id/deposit', authorize([UserRole.ADMIN]), async function (
 })
 
 router.post('/:wallet_id/withdraw', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-    const keylist: string[] = ['transaction_id', 'address_id', 'amount'];
+    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
     if (isNotSafe(keylist, req.body)) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
@@ -46,7 +53,7 @@ router.post('/:wallet_id/withdraw', authorize([UserRole.ADMIN]), async function 
 })
 
 router.post('/:wallet_id/borrow', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-    const keylist: string[] = ['transaction_id', 'address_id', 'amount'];
+    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
     if (isNotSafe(keylist, req.body)) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
@@ -55,7 +62,7 @@ router.post('/:wallet_id/borrow', authorize([UserRole.ADMIN]), async function (r
 })
 
 router.post('/:wallet_id/payback', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-    const keylist: string[] = ['transaction_id', 'address_id', 'amount'];
+    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
     if (isNotSafe(keylist, req.body)) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
@@ -64,7 +71,7 @@ router.post('/:wallet_id/payback', authorize([UserRole.ADMIN]), async function (
 })
 
 router.post('/:wallet_id/stake', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-    const keylist: string[] = ['transaction_id', 'address_id', 'amount'];
+    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
     if (isNotSafe(keylist, req.body)) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
@@ -77,7 +84,7 @@ router.post('/:wallet_id/swap', authorize([UserRole.ADMIN]), async function (req
     if (swaplist.length != 2)
         return res.status(400).send({ error: 'Request body must be a list with two transaction' });
 
-    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'conversion_rate'];
+    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'conversion_rate', 'status', 'slot'];
     if (isNotSafe(keylist, swaplist[0])) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
@@ -89,7 +96,7 @@ router.post('/:wallet_id/swap', authorize([UserRole.ADMIN]), async function (req
 })
 
 router.post('/:wallet_id/reward', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'base_address_id'];
+    const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'base_address_id', 'status', 'slot'];
     if (isNotSafe(keylist, req.body)) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
