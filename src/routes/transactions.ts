967ba63ff727a85f1de347d6328cc9ded8ab330d
Ghassan Hallaq <gus.hallaq@gmail.com>
2022-02-17 09:58:18 -0500

    add new param

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -34,7 +34,7 @@ router.get('/:wallet_id/:signature', async function (req: Request, res: Response
     });
 })
 
-router.post('/:wallet_id/add', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
+router.post('/:wallet_id/new',/* authorize([UserRole.ADMIN]),*/ async function (req: Request, res: Response) {
     const keylist: string[] = ['tx_type', 'signature'];
     if (isNotSafe(keylist, req.body)) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
@@ -42,66 +42,5 @@ router.post('/:wallet_id/add', authorize([UserRole.ADMIN]), async function (req:
     let result = await addTransaction(req.params.wallet_id, req.body);
     res.send(JSON.stringify(result));
 })
-
-// router.post('/:wallet_id/withdraw', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-//     const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
-//     if (isNotSafe(keylist, req.body)) {
-//         return res.status(400).send({ error: 'Request body missing some parameters' });
-//     }
-//     let result = await addWithdraw(req.params.wallet_id, req.body);
-//     res.send(JSON.stringify(result));
-// })
-
-// router.post('/:wallet_id/borrow', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-//     const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
-//     if (isNotSafe(keylist, req.body)) {
-//         return res.status(400).send({ error: 'Request body missing some parameters' });
-//     }
-//     let result = await addBorrow(req.params.wallet_id, req.body);
-//     res.send(JSON.stringify(result));
-// })
-
-// router.post('/:wallet_id/payback', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-//     const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
-//     if (isNotSafe(keylist, req.body)) {
-//         return res.status(400).send({ error: 'Request body missing some parameters' });
-//     }
-//     let result = await addPayback(req.params.wallet_id, req.body);
-//     res.send(JSON.stringify(result));
-// })
-
-// router.post('/:wallet_id/stake', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-//     const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'status', 'slot'];
-//     if (isNotSafe(keylist, req.body)) {
-//         return res.status(400).send({ error: 'Request body missing some parameters' });
-//     }
-//     let result = await addStake(req.params.wallet_id, req.body);
-//     res.send(JSON.stringify(result));
-// })
-
-// router.post('/:wallet_id/swap', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-//     const swaplist: {}[] = req.body;
-//     if (swaplist.length != 2)
-//         return res.status(400).send({ error: 'Request body must be a list with two transaction' });
-
-//     const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'conversion_rate', 'status', 'slot'];
-//     if (isNotSafe(keylist, swaplist[0])) {
-//         return res.status(400).send({ error: 'Request body missing some parameters' });
-//     }
-//     if (isNotSafe(keylist, swaplist[1])) {
-//         return res.status(400).send({ error: 'Request body missing some parameters' });
-//     }
-//     let result = await addSwap(req.params.wallet_id, req.body);
-//     res.send(JSON.stringify(result));
-// })
-
-// router.post('/:wallet_id/reward', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-//     const keylist: string[] = ['transaction_id', 'address_id', 'amount', 'base_address_id', 'status', 'slot'];
-//     if (isNotSafe(keylist, req.body)) {
-//         return res.status(400).send({ error: 'Request body missing some parameters' });
-//     }
-//     let result = await addReward(req.params.wallet_id, req.body);
-//     res.send(JSON.stringify(result));
-// })
 
 module.exports = router
