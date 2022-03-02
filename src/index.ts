4f951ff53fdebab8d73b74244b4bd398b2385a58
Ghassan Hallaq <gus.hallaq@gmail.com>
2022-03-02 06:07:33 -0500

    sync with dev

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,6 @@ import { cacheInit } from './api/cacheList'
 
 let bodyParser = require('body-parser')
 
-// const https = require('https');
-// const fs = require('fs');
-
 let platforms = require('./routes/platforms');
 let lpairs = require('./routes/lpairs');
 let tokens = require('./routes/tokens');
@@ -23,13 +20,14 @@ const app = express();
 const allowedOrigins: Array<string | RegExp> = process.env.API_CORS_ALLOWED_ORIGINS?.split(',') || ['http://localhost:3000'];
 allowedOrigins.push(/\.netlify.app$/);
 
-app.use(bodyParser.json());
-
 const options: cors.CorsOptions = {
     origin: allowedOrigins
 };
 console.log(options);
 app.use(cors(options));
+
+app.use(bodyParser.json());
+
 app.use(express.json());
 
 app.use('/platforms', platforms);
@@ -49,10 +47,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Ratio Finance Backend listening on port ${port}`)
 })
-
-// https.createServer({
-//     key: fs.readFileSync('/etc/letsencrypt/live/backend.ratio.finance/privkey.pem'),
-//     cert: fs.readFileSync('/etc/letsencrypt/live/backend.ratio.finance/fullchain.pem'),
-// }, app).listen(port, () => {
-//     console.log(`HTTPS Server running on port ${port}`);
-// });
