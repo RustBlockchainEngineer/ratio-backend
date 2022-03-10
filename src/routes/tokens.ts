53c6188e5c2a181f17f17cffc372e46d6e1fdb4c
Ghassan Hallaq <gus.hallaq@gmail.com>
2022-03-10 06:45:09 -0500

    add token platforms

--- src/routes/tokens.ts
+++ src/routes/tokens.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { getAllTokens, getToken, addToken, addNewPriceToken, getLatestTokenPrice, updateToken, deleteToken } from '../api/tokens'
+import { getAllTokens, getToken, addToken, deleteToken } from '../api/tokens'
 import { isNotSafe } from '../utils/utils';
 import { authorize } from '../middlewares/auth';
 import { UserRole } from '../models/model';
@@ -18,11 +18,12 @@ router.get('/:id', async function (req: Request, res: Response) {
         res.send(JSON.stringify(result));
     });
 })
-router.get('/:id/price', async function (req: Request, res: Response) {
-    await getLatestTokenPrice(req.params.id, function (result) {
-        res.send(JSON.stringify(result));
-    });
-})
+
+// router.get('/:id/price', async function (req: Request, res: Response) {
+//     await getLatestTokenPrice(req.params.id, function (result) {
+//         res.send(JSON.stringify(result));
+//     });
+// })
 
 router.post('/', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
     const keylist: string[] = ['address_id', 'symbol', 'icon'];
@@ -30,25 +31,25 @@ router.post('/', authorize([UserRole.ADMIN]), async function (req: Request, res:
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
 
-    let result = await addToken(req.body);
+    let result = await addToken(req.body["address_id"],req.body);
     res.send(JSON.stringify(result));
 })
 
-router.post('/:id/price', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
-    const keylist: string[] = ['price', 'confidence'];
-    if (isNotSafe(keylist, req.body)) {
-        return res.status(400).send({ error: 'Request body missing some parameters' });
-    }
-    let result = await addNewPriceToken(req.params.id, req.body);
-    res.send(JSON.stringify(result));
-})
+// router.post('/:id/price', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
+//     const keylist: string[] = ['price', 'confidence'];
+//     if (isNotSafe(keylist, req.body)) {
+//         return res.status(400).send({ error: 'Request body missing some parameters' });
+//     }
+//     let result = await addNewPriceToken(req.params.id, req.body);
+//     res.send(JSON.stringify(result)); 
+// })
 
 router.put('/:id', authorize([UserRole.ADMIN]), async function (req: Request, res: Response) {
     const keylist: string[] = ['symbol', 'icon'];
     if (isNotSafe(keylist, req.body)) {
         return res.status(400).send({ error: 'Request body missing some parameters' });
     }
-    let result = await updateToken(req.params.id, req.body);
+    let result = await addToken(req.params.id, req.body);
     res.send(JSON.stringify(result));
 })
 
