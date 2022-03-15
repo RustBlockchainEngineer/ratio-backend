88ffe2494dd8809efe002d68830ea505eecc80a4
Alejo Lovallo <alejoe.lovallo@gmail.com>
2022-03-15 10:30:48 -0300

    rfm 1013 without usdr calculation

--- src/api/saber.ts
+++ src/api/saber.ts
@@ -23,8 +23,44 @@ const getConnection = async(env:NETWORK) => {
   }
 }
 
-async function fetchSaberTokens(network: string) {
+const fetchSaberPools = async (network:string) => {
     const poolsData = (await Axios.get(`https://registry.saber.so/data/pools-info.${network}.json`)).data.pools;
+    return poolsData;
+}
+
+const parsePoolData = (pool:any) => {
+    const name = pool?.name;
+    const tokenAName = name.split('-')[0];
+    const tokenBName = name.split('-')[1];
+    const tokenA = pool?.swap?.state?.tokenA?.reserve;
+    const tokenACoinGeckoId = pool?.tokens[0]?.extensions?.coingeckoId;
+    const tokenB = pool?.swap?.state?.tokenB?.reserve;
+    const tokenBCoinGeckoId = pool?.tokens[1]?.extensions?.coingeckoId;
+    return{
+      name,
+      tokenA,
+      tokenAName,
+      tokenACoinGeckoId,
+      tokenBName,
+      tokenB,
+      tokenBCoinGeckoId,
+      tokenASize: '',
+      tokenBSize: ''
+    }
+}
+
+export const fetchSaberPoolData = async (network: string, poolName: string) => {
+    const poolsData = await fetchSaberPools(network);
+    const poolData = poolsData.find( (pool:any) => pool?.name == poolName);
+    if(poolData){
+      return parsePoolData(poolData);
+    }else{
+      return undefined;
+    }
+}
+
+async function fetchSaberTokens(network: string) {
+    const poolsData = await fetchSaberPools(network);
     const swapPools = [];
     for (let i = 0; i < poolsData.length; i++) {
       const name = poolsData[i]?.name;
@@ -47,7 +83,7 @@ async function fetchSaberTokens(network: string) {
     return swapPools;
 }
 
-const getPoolTokenSizes = async(pools:any, env:NETWORK) => {
+const getPoolsTokenSizes = async(pools:any, env:NETWORK) => {
   const connection = await getConnection(env);
   const poolsFormatted = await Promise.all(
     pools.map(async (pool:any) => {
@@ -59,7 +95,43 @@ const getPoolTokenSizes = async(pools:any, env:NETWORK) => {
   return poolsFormatted;
 }
 
+export const getPoolTokenSizes = async(pool:any, env:NETWORK) => {
+  const connection = await getConnection(env);
+  pool.tokenASize = (await getAccount(connection,new PublicKey(pool.tokenA))).amount.toString();
+  pool.tokenBSize = (await getAccount(connection,new PublicKey(pool.tokenB))).amount.toString();
+  return pool;
+}
+
 export const getSaberLpTokenPrice = async (env:NETWORK,poolName: string) => {
+  const pool = await fetchSaberPoolData(env,poolName);
+  const tokenPrices = tokenPriceList;
+  let poolPrice;
+  if(pool){
+    const poolFormatted = await getPoolTokenSizes(pool,env);
+    if(tokenPrices[poolFormatted.tokenAName] != undefined && tokenPrices[poolFormatted.tokenBName]!= undefined){
+      //@ts-ignore
+      const tokenAPriceSqrt = (new BigNumber(tokenPrices[poolFormatted.tokenAName])).sqrt();
+      //@ts-ignore
+      const tokenBPriceSqrt = (new BigNumber(tokenPrices[poolFormatted.tokenBName])).sqrt();
+      const tokenASizeSqrt =  (new BigNumber(poolFormatted.tokenASize)).sqrt();
+      const tokenBSizeSqrt =  (new BigNumber(poolFormatted.tokenBSize)).sqrt();
+      const totalReserveTokens = (new BigNumber(poolFormatted.tokenASize)).plus(poolFormatted.tokenBSize);
+
+      poolPrice = ( (tokenAPriceSqrt
+                          .multipliedBy(tokenBPriceSqrt)
+                          .multipliedBy(tokenASizeSqrt)
+                          .multipliedBy(tokenBSizeSqrt)
+                        ).multipliedBy(2) ).dividedBy(totalReserveTokens).toString();
+    }
+    return {
+      poolName: poolFormatted.name,
+      lpPrice: poolPrice,
+      tokenASize: poolFormatted.tokenASize,
+      tokenBSize: poolFormatted.tokenBSize,
+      tokenAPrice:tokenPrices[poolFormatted.tokenAName],
+      tokenBPrice:tokenPrices[poolFormatted.tokenBName]
+    };
+  }
 }
 /**
  * 
@@ -70,7 +142,7 @@ export const getSaberLpTokenPrice = async (env:NETWORK,poolName: string) => {
 export const getSaberLpTokenPrices = async(env: NETWORK) => {
     const pools = await fetchSaberTokens(env);
     const tokenPrices = tokenPriceList;
-    const poolTokenSizes = await getPoolTokenSizes(pools,env);
+    const poolTokenSizes = await getPoolsTokenSizes(pools,env);
     const lpPrices = await Promise.all(
         poolTokenSizes.map(async (pool:any) => {
           //@ts-ignore
--- src/routes/saberPoolSizes.ts
+++ src/routes/saberPoolSizes.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from 'express';
-import {getSaberLpTokenPrices,NETWORK} from '../api/saber';
+import {getSaberLpTokenPrice, getSaberLpTokenPrices,NETWORK,} from '../api/saber';
 let router = express.Router();
 
 router.get('/', async function (req: Request, res: Response) {
     let result = await getSaberLpTokenPrices(NETWORK.DEVNET);
     res.send(JSON.stringify(result));
 })
+
+router.get('/:id', async function(req: Request, res: Response) {
+    let result = await getSaberLpTokenPrice(NETWORK.DEVNET,req.params.id);
+    if(result){
+        res.send(JSON.stringify(result))
+    }else{
+        res.status(404).send({ error: 'POOL NOT FOUND'});
+    }
+});
 
 module.exports = router
