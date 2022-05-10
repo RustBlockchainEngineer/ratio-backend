a40ba8da50104fe1454825630e2a409a53621e23
Ghassan Hallaq <gus.hallaq@gmail.com>
2022-05-10 07:24:41 -0400

    add lpair status

--- src/routes/lpairs.ts
+++ src/routes/lpairs.ts
@@ -8,12 +8,12 @@ import { authorize } from '../middlewares/auth';
 let router = express.Router();
 
 router.get('/', async function (req: Request, res: Response) {
-    let result = await getAllLPairs(function (result) {
+    let result = await getAllLPairs(res.locals.user.role,function (result) {
         res.send(JSON.stringify(result));
     });
 })
 router.get('/:id', async function (req: Request, res: Response) {
-    let result = await getLPair(req.params.id, function (result) {
+    let result = await getLPair(res.locals.user.role,req.params.id, function (result) {
         if (result)
             res.send(JSON.stringify(result));
         else
